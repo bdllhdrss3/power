7d9d6111544757e7272b03f50279d90690ae0312
unknown <bdllhltydrss@gmail.com>
2019-01-28 11:28:32 +0300

    d

---
+++ power.py
@@ -0,0 +1,14 @@
+#creating the power app
+       #craeting inputs
+value = int(input('Enter the digit : '))
+power = int(input('Enter the raising power : '))
+       #craeting the power function
+pass
+def power_function(value,power):
+    answer = value**power
+    print(answer)
+    
+
+power_function(value,power)
+
+    
